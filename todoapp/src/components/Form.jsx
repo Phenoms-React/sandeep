dba27cdbb7cd850a5780b88865116d4ca19d9482
patis <Sandeep.Patil@in.pega.com>
2024-04-13 17:42:43 +0530

    Completed TodoApp

--- todoapp/src/components/Form.jsx
+++ todoapp/src/components/Form.jsx
@@ -1,21 +1,28 @@
 import { useState } from "react";
+import styles from "./form.module.css";
 
 export default function Form({ todos, setTodos }) {
-  const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState({ name: "", done: false });
 
   function handleSubmit(e) {
     e.preventDefault();
     setTodos([...todos, todo]);
-    setTodo("");
+    setTodo({ name: "", done: false });
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        onChange={(e) => setTodo(e.target.value)}
-        value={todo}
-        type="text"
-      />
-      <button type="submit">Add</button>
+    <form className={styles.todoform} onSubmit={handleSubmit}>
+      <div className={styles.inputContainer}>
+        <input
+          className={styles.modernInput}
+          onChange={(e) => setTodo({ name: e.target.value, done: false })}
+          value={todo.name}
+          type="text"
+          placeholder="Enter Todo item"
+        />
+        <button className={styles.modernButton} type="submit">
+          Add
+        </button>
+      </div>
     </form>
   );
 }
--- todoapp/src/components/TodoList.jsx
+++ todoapp/src/components/TodoList.jsx
@@ -1,10 +1,19 @@
 import Todoitem from "./Todoitem";
+import styles from "./todolist.module.css";
 
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, setTodos }) {
+  const sortedTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
-    <div>
-      {todos.map((item) => (
-        <Todoitem key={item} item={item} />
+    <div className={styles.list}>
+      {sortedTodos.map((item) => (
+        <Todoitem
+          key={item.name}
+          item={item}
+          todos={todos}
+          setTodos={setTodos}
+        />
       ))}
     </div>
   );
